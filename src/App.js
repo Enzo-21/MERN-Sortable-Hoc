3f09fb51aa30749b8c4ee8525b4059817ab73ffe
Enzo Barrera <enzo@MacBook-Air-de-Enzo.local>
2020-05-25 16:54:44 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc'
+import arrayMove from 'array-move'
+import axios from 'axios'
+import { BASE_URL } from './config'
+import Navbar from './Navbar';
+
+import './App.css'
+
+const SortableItem = SortableElement(({ title, description, _id }) => {
+  return (
+    <li className='list-group-item item my-3'>
+      
+  <h1>{title}</h1>
+      <p>{description}</p>
+  <p>{_id}</p>
+    </li>
+  )
+})
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul className='list-group'>
+      {items.map((task, i) => (
+        <SortableItem {...task} index={i} key={i} /> //Using spread operator to pass a copy of the task props 
+      ))}
+    </ul>
+  )
+})
+
+const SortableTasks = () => {
+  const [tasks, setTasks] = useState([])
+
+  const fetchData = async () => {
+    const {data} = await axios.get(`${BASE_URL}tasks`)
+    
+    data.sort((a,b) => (a.order > b.order) ? 1 : ((b.order > a.order) ? -1 : 0))
+    
+    setTasks(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const onSortEnd = async ({ oldIndex, newIndex }) => {
+
+    let tasksCopy = [...tasks]
+    tasksCopy = arrayMove(tasksCopy, oldIndex, newIndex)
+    setTasks(tasksCopy)
+
+    const taskIds = tasksCopy.map(task => task._id)
+
+
+    const res = await axios({
+      method: 'put',
+      url: `${BASE_URL}tasks`,
+      data: taskIds
+    });
+
+    const data = await res
+
+    console.log(data);
+    
+    console.log(taskIds);
+
+  }
+
+  return (
+    <SortableList items={tasks} onSortEnd={onSortEnd} />
+  )
+}
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+ 
+    
+     <div className="container">
+       <div className="jumbotron text-center mt-4 p-2">
+       <h4>How it works... 🤔</h4>
+     <p>You can drag the elements to sort them as you like, they will be automatically updated on the database 😄</p>
+       </div>
+  
+      <div className="row">
+        <div className="col-12">
+          <SortableTasks />
+        </div>
+
+      </div>
+    </div>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav class="navbar text-center">
+  <span class=" mb-0 p-1 h2 ">MERN Stack App - "React Sortable Hoc"</span>
+</nav>
+    )
+}
+
+export default Navbar
